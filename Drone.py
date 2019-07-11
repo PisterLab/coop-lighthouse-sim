9ae44fe6553f40fe3d2dab4de419fdb6b36bfde9
Avery Liou <liou.avery@berkeley.edu>
2019-07-11 15:52:43 -0700

    Added state class

--- Drone.py
+++ Drone.py
@@ -4,3 +4,5 @@ class DroneType(enum.Enum):
     lighthouse_robot = enum.auto()      # localizing itself and localizing anchor robots
     measurement_robot = enum.auto()     # only taking measurements
     anchor_robot = enum.auto()      # in place, acts as an anchor point
+
+def __init__(self, x=5, y=5, theta=0, vx=0, vy=0, drone_type=DroneType.measurement_robot):
